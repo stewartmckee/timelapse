3bc627ac7c277c110e650ab3669928513d0e970c
Stewart McKee <stewart@theizone.co.uk>
2013-03-24 20:29:22 +0000

    plumbed in gphoto4ruby

--- lib/timelapse.rb
+++ lib/timelapse.rb
@@ -1,5 +1,31 @@
-require "timelapse/version"
+#require "timelapse/version"
+require 'rufus/scheduler'
+require 'gphoto4ruby'
 
 module Timelapse
-  # Your code goes here...
+  class Session
+
+    def start(options={})
+      begin
+        options[:interval] = "30s" unless options.has_key?(:interval)
+        save_options = {:to_folder => options[:output]} if options.has_key?(:output)
+        puts "Connecting to camera..."
+        camera = GPhoto2::Camera.new
+        puts "Using #{camera.model_name}"
+        puts "Interval: #{options[:interval]}"
+        puts "Output: #{options[:output]}" if options[:output]
+        scheduler = Rufus::Scheduler.start_new
+        scheduler.every options[:interval] do
+          puts "Taking picture..."
+          camera.capture.save(save_options).delete
+          puts "Saved."
+        end
+        scheduler.join
+      rescue => e
+        puts e
+      ensure
+        camera.dispose
+      end
+    end
+  end
 end
--- timelapse.gemspec
+++ timelapse.gemspec
@@ -20,4 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_runtime_dependency "gphoto4ruby"
+  spec.add_runtime_dependency "rufus-scheduler"
+  spec.add_runtime_dependency "slop"
 end
